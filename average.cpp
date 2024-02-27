d07cb24315283cbc66df4bf7a1b670451cb7085b
PhichaPrim <phichamon_duangmanee@cmu.ac.th>
2024-02-27 13:01:15 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    double temp, avr, sum = 0;
+    for(int i=0; i<argc-1; i++){
+        temp = atof(argv[i+1]);
+        sum += temp;
+    }
+    avr = sum/(argc-1);
+    if(argc==1) cout << "Please input numbers to find average.";
+    else{
+        cout <<"---------------------------------\n";
+        cout << "Average of "<< argc-1 <<" numbers = "<< avr <<endl;
+        cout <<"---------------------------------\n";
+    }
+    return 0;
+}
